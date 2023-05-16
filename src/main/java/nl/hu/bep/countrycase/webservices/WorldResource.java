3937867060f3f2c9386e3e4470d49bc881e0c789
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2023-05-16 10:55:16 +0200

    Practicum 6.1

--- src/main/java/nl/hu/bep/countrycase/webservices/WorldResource.java
+++ src/main/java/nl/hu/bep/countrycase/webservices/WorldResource.java
@@ -3,18 +3,74 @@ package nl.hu.bep.countrycase.webservices;
 import nl.hu.bep.countrycase.model.Country;
 import nl.hu.bep.countrycase.model.World;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.json.*;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.StringReader;
 import java.util.List;
 
 @Path("/countries")
 public class WorldResource {
+
+    @POST
+    @Path("/demo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String postMessage(String jsonBody) {
+        StringReader strReader = new StringReader(jsonBody);
+        JsonReader jsonReader = Json.createReader(strReader);
+
+        JsonArray students = jsonReader.readArray();
+
+        for (JsonValue student : students) {
+            var naam = ((JsonObject)student).getString("voornaam");
+            var leeftijd = ((JsonObject)student).getJsonNumber("leeftijd").intValue();
+
+            System.out.println(naam);
+            System.out.println(leeftijd);
+        }
+
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("result", "Message received!");
+        return job.build().toString();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String postCountry(String jsonBody) {
+        StringReader strReader = new StringReader(jsonBody);
+        JsonReader jsonReader = Json.createReader(strReader);
+        JsonObject newCountry = jsonReader.readObject();
+
+        String code = newCountry.getString("code");
+        String iso3 = newCountry.getString("iso3");
+        String name = newCountry.getString("name");
+        String capital = newCountry.getString("capital");
+        String continent = newCountry.getString("continent");
+        String region = newCountry.getString("region");
+        double surface = Double.parseDouble(newCountry.getString("surface"));
+        int population = Integer.parseInt(newCountry.getString("population"));
+        String government = newCountry.getString("government");
+        double latitude = Double.parseDouble(newCountry.getString("latitude"));
+        double longitude = Double.parseDouble(newCountry.getString("longitude"));
+
+        List<Country> countries = World.getWorld().getAllCountries();
+        for (Country country : countries) {
+            if (country.getCode().equals(code)) {
+                JsonObjectBuilder job = Json.createObjectBuilder();
+                job.add("result", "Country already exists!");
+                return job.build().toString();
+            }
+        }
+
+        Country c = new Country(code, iso3, name, capital, continent, region, surface, population, government, latitude, longitude);
+        countries.add(c);
+
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("result", "Country added!");
+        return job.build().toString();
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
